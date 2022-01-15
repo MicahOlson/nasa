9d827e59a56fd807a67050abff30363bdf5baaaf
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2022-01-14 18:22:30 -0800

    Resolve security vulnerabilities in dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,47 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'eval-source-map',
   devServer: {
-    contentBase: './dist'
+    static: './dist',
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new Dotenv(),
+    new ESLintPlugin(),
     new HtmlWebpackPlugin({
       title: 'Nasa',
       template: './src/index.html',
-      inject: 'body'
+      inject: 'body',
     }),
-    new Dotenv()
   ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: 'asset/resource',
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader"
+        test: /\.html$/,
+        use: ['html-loader'],
       },
       {
-        test: /\.(gif|png|jpe?g)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/images/'
-            }
-          }
-        ]
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test:/\.html$/,
-        use: [
-          'html-loader'
-        ]
-      }
-    ]
-  }
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
